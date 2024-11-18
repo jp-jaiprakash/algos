924cf71bbf9e8649e5cf3139284a39d3173f056e
Jai Prakash <jaiprakash@Jais-MacBook-Air.local>
2024-11-19 07:33:09 +0800

    Sliding Window: SmallestSubArrayWithGreaterSum

---
+++ src/main/java/org/example/slidingwindows/SmallestSubArrayWithGreaterSum.java
@@ -0,0 +1,32 @@
+package org.example.slidingwindows;
+
+public class SmallestSubArrayWithGreaterSum {
+    public int solve(int[] inputArray, int targetSum) {
+
+        if (inputArray== null || inputArray.length == 0)
+            return 0;
+
+        if(inputArray.length == 1 ){
+            if( inputArray[0] ==targetSum)
+                return 1;
+            else
+                return 0;
+        }
+
+        int windowSum =0, minLength = Integer.MAX_VALUE, windowStart =0;
+
+        for(int windowEnd=0; windowEnd < inputArray.length; windowEnd++){
+            windowSum += inputArray[windowEnd];
+
+            while(windowSum >= targetSum){
+                int currentLength = windowEnd - windowStart + 1;
+                if(minLength > currentLength)
+                    minLength = currentLength;
+
+                windowSum -=inputArray[windowStart];
+                windowStart++;
+            }
+        }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+}
---
+++ src/test/java/slidingwindows/SmallestSubArrayWithGreaterSumTest.java
@@ -0,0 +1,45 @@
+package slidingwindows;
+
+import org.example.slidingwindows.SmallestSubArrayWithGreaterSum;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SmallestSubArrayWithGreaterSumTest {
+
+    /**
+     * Empty array
+     * single element array and value less than target
+     * smallest subarray = 1 (single element equal)
+     * Typical Case: S=7, arr = 2,3,1,2,4,3
+     * No valid subarray
+     * All elements needed
+     * all elements same
+     */
+
+    @ParameterizedTest
+    @CsvSource({
+            "'', 7, 0",
+            "'5',5,1",
+            "'5',6,0",
+            "'2,3,1,2,4,3', 7 ,2",
+            "'2,3,1,2,4,3', 700 ,0",
+            "'2,3,1,2,4,3', 15 ,6",
+            "'2,2,2,2,2,2', 4 ,2",
+
+    })
+    void TestSolution(String inputArray, int targetSum, int answer) {
+        SmallestSubArrayWithGreaterSum smallestSubArrayWithGreaterSum = new SmallestSubArrayWithGreaterSum();
+        int[] array = parseArray(inputArray);
+        int actualAnswer = smallestSubArrayWithGreaterSum.solve(array, targetSum);
+
+        assertEquals(actualAnswer, answer);
+    }
+
+    private int[] parseArray(String input) {
+        if (input.isEmpty())
+            return null;
+        return java.util.Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
+    }
+}
