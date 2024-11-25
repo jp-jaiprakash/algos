b81388b496dfdb777a1b95f2adf37e05f975ea75
Jai Prakash <jaiprakash@Jais-MacBook-Air.local>
2024-11-26 04:49:54 +0800

    SmallestSubArrayWithGeaterSum

--- src/main/java/org/example/slidingwindows/SmallestSubArrayWithGreaterSum.java
+++ src/main/java/org/example/slidingwindows/SmallestSubArrayWithGreaterSum.java
@@ -3,27 +3,27 @@ package org.example.slidingwindows;
 public class SmallestSubArrayWithGreaterSum {
     public int solve(int[] inputArray, int targetSum) {
 
-        if (inputArray== null || inputArray.length == 0)
+        if (inputArray == null || inputArray.length == 0)
             return 0;
 
-        if(inputArray.length == 1 ){
-            if( inputArray[0] ==targetSum)
+        if (inputArray.length == 1) {
+            if (inputArray[0] == targetSum)
                 return 1;
             else
                 return 0;
         }
 
-        int windowSum =0, minLength = Integer.MAX_VALUE, windowStart =0;
+        int windowSum = 0, minLength = Integer.MAX_VALUE, windowStart = 0;
 
-        for(int windowEnd=0; windowEnd < inputArray.length; windowEnd++){
+        for (int windowEnd = 0; windowEnd < inputArray.length; windowEnd++) {
             windowSum += inputArray[windowEnd];
 
-            while(windowSum >= targetSum){
+            while (windowSum >= targetSum) {
                 int currentLength = windowEnd - windowStart + 1;
-                if(minLength > currentLength)
+                if (minLength > currentLength)
                     minLength = currentLength;
 
-                windowSum -=inputArray[windowStart];
+                windowSum -= inputArray[windowStart];
                 windowStart++;
             }
         }
